1b3a066c85268745e4a356958a5bb6786cb377b8
ComarPers922 <18982127626@163.com>
2017-04-17 23:53:10 -0400

    Initial Commit

---
+++ GestureAndGraph/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GestureAndGraph
+//
+//  Created by ComarPers 922 on 2017/4/17.
+//  Copyright © 2017年 ComarPers 922. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GestureAndGraph/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GestureAndGraph
+//
+//  Created by ComarPers 922 on 2017/4/17.
+//  Copyright © 2017年 ComarPers 922. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
